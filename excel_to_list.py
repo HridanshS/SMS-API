cfc9bd6437578056b1d23102e634ff37759033ea
Hridansh Saraogi <78390395+HridanshS@users.noreply.github.com>
2021-05-07 09:57:12 -0400

    Update excel_to_list.py
    Made code easier to read

--- excel_to_list.py
+++ excel_to_list.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 #Insert complete path to the excel file and index of the worksheet
-df = pd.read_excel("/Users/Shared/cs130R/message.py/EEVM - Camapign texts.xlsx", engine= 'openpyxl',sheet_name=0)
+df = pd.read_excel("/Users/Shared/cs130R/message.py/file_name.xlsx", engine= 'openpyxl',sheet_name=0)
 # insert the name of the column as a string in brackets
 list1 = list(df['Name']) 
 list2 =list(df['Phone'])
